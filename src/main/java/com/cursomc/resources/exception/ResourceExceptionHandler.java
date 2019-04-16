0459a8b2533bbe0975a0b2c2da0034b5a30f575a
Railson <railson170@hotmail.com>
2019-04-16 15:39:57 -0300

    Praticando: Personalizar mensagem validation

--- src/main/java/com/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.cursomc.resources.exception;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,16 +13,29 @@ import com.cursomc.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest req){
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest req) {
 		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
+
 	@ExceptionHandler(DataIntegretyException.class)
-	public ResponseEntity<StandardError> dataIntegrety(DataIntegretyException e, HttpServletRequest req){
+	public ResponseEntity<StandardError> dataIntegrety(DataIntegretyException e, HttpServletRequest req) {
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest req) {
+		
+		ValidationError validationError = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			validationError.addError(f.getField(),f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(validationError);
+	}
+
 }
